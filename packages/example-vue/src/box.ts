7585b2e2c6d9d076952da3576ec32a07e81f7842
湫曗 <autumnsilverleaves@gmail.com>
2024-09-08 00:29:26 +0800

    feat: support 多次反弹检测

--- packages/example-vue/src/box.ts
+++ packages/example-vue/src/box.ts
@@ -27,34 +27,50 @@ export class CubeBox {
   }
 
   adjustChildrenStatus() {
-    this.spriteList.forEach((item) => {
-      const bottom = 0
-      const itemBottom = item.graphics.y - item.r
-      if (itemBottom < bottom) {
-        item.graphics.y = bottom + Math.abs(itemBottom - bottom) * 0.9 + item.r
-        item.vy = 0.9 * Math.abs(item.vy)
+    let bouncedList: Atomic[] = this.spriteList
+    // 1 重反弹
+    for (let i = 0; bouncedList.length; i++) {
+      if (i > 1) {
+        throw new Error('出现超过 1 次反弹情况', {
+          cause: bouncedList,
+        })
       }
+      bouncedList = bouncedList.filter((item) => {
+        const bottom = 0
+        const itemBottom = item.graphics.y - item.r
+        if (itemBottom < bottom) {
+          item.graphics.y =
+            bottom + Math.abs(itemBottom - bottom) * 0.9 + item.r
+          item.vy = 0.9 * Math.abs(item.vy)
+          return true
+        }
 
-      const left = 0
-      const itemLeft = item.graphics.x - item.r
-      if (itemLeft < left) {
-        item.graphics.x = left + Math.abs(itemLeft - left) * 0.9 + item.r
-        item.vx = 0.9 * Math.abs(item.vx)
-      }
+        const left = 0
+        const itemLeft = item.graphics.x - item.r
+        if (itemLeft < left) {
+          item.graphics.x = left + Math.abs(itemLeft - left) * 0.9 + item.r
+          item.vx = 0.9 * Math.abs(item.vx)
+          return true
+        }
 
-      const top = this.h
-      const itemTop = item.graphics.y + item.r
-      if (itemTop > top) {
-        item.graphics.y = top - Math.abs(itemTop - top) * 0.9 - item.r
-        item.vy = -0.9 * Math.abs(item.vy)
-      }
+        const top = this.h
+        const itemTop = item.graphics.y + item.r
+        if (itemTop > top) {
+          item.graphics.y = top - Math.abs(itemTop - top) * 0.9 - item.r
+          item.vy = -0.9 * Math.abs(item.vy)
+          return true
+        }
 
-      const right = this.w
-      const itemRight = item.graphics.x + item.r
-      if (itemRight > right) {
-        item.graphics.x = right - Math.abs(itemRight - right) * 0.9 - item.r
-        item.vx = -0.9 * Math.abs(item.vx)
-      }
-    })
+        const right = this.w
+        const itemRight = item.graphics.x + item.r
+        if (itemRight > right) {
+          item.graphics.x = right - Math.abs(itemRight - right) * 0.9 - item.r
+          item.vx = -0.9 * Math.abs(item.vx)
+          return true
+        }
+
+        return false
+      })
+    }
   }
 }
