17613bd64cfd469319f69d9b759b8af381c025fc
湫曗 <autumnsilverleaves@gmail.com>
2024-09-08 00:16:49 +0800

    fix: fix bottom logic

--- packages/example-vue/src/box.ts
+++ packages/example-vue/src/box.ts
@@ -29,18 +29,25 @@ export class CubeBox {
   adjustChildrenStatus() {
     this.spriteList.forEach((item) => {
       const bottom = 0
-      if (item.graphics.y - item.r < bottom) {
-        item.graphics.y =
-          bottom + item.r + Math.abs(item.graphics.y - bottom) * 0.9
+      const itemBottom = item.graphics.y - item.r
+      if (itemBottom < bottom) {
+        item.graphics.y = bottom + Math.abs(itemBottom - bottom) * 0.9 + item.r
         item.vy = 0.9 * Math.abs(item.vy)
       }
 
-      const left = 0
+      // const left = 0
+      // if (item.graphics.x - item.r < left) {
+      //   item.graphics.x = left + item.r + Math.abs(item.graphics.x - left) * 0.9
+      //   item.vx = 0.9 * Math.abs(item.vx)
+      // }
 
-      if (item.graphics.x - item.r < left) {
-        item.graphics.x = left + item.r + Math.abs(item.graphics.x - left) * 0.9
-        item.vx = 0.9 * Math.abs(item.vx)
-      }
+      // const top = this.h
+      // const itemTop = item.graphics.y + item.r
+      // if ( itemTop > top) {
+      //   item.graphics.y =
+      //   top - (Math.abs(itemTop - top) * 0.9)
+      //   item.vy = -0.9 * Math.abs(item.vy)
+      // }
     })
   }
 }
