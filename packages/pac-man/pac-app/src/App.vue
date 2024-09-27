53ce0c6f36654d2bfba6b849f0d012a6b56bf1ec
Yukiminifire <756440057@qq.com>
2024-09-27 12:37:05 +0800

    feat: add pac

---
+++ packages/pac-man/pac-app/src/App.vue
@@ -0,0 +1,122 @@
+<template>
+  <div>
+    <div>
+      <h1 class="text-center text-lg m-8">五子棋 🐲 vs 🐯 争霸赛</h1>
+    </div>
+    <div class="board-container">
+      <div class="board">
+        <div v-for="(row, y) in board" class="row">
+          <div
+            v-for="(col, x) in row"
+            class="coin"
+            @click="setPiece(col, x, y)"
+          >
+            <div
+              v-if="col.color !== EPieceType.empty"
+              :class="`pill ${col.color}`"
+            ></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-if="isWin" class="text-center text-lg m-8">{{ userColor }} win!</div>
+    <div class="text-center text-lg m-8">
+      <button @click="reset">reset</button>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+import {
+  genBoard,
+  EPieceType,
+  IPosition,
+  positionWinCheck,
+} from '@template/pac-board'
+
+const board = ref(genBoard())
+
+const userColor = ref<EPieceType>(EPieceType.black)
+const isWin = ref<boolean>(false)
+
+function reset() {
+  board.value = genBoard()
+
+  userColor.value = EPieceType.black
+  isWin.value = false
+}
+
+function setPiece(col: IPosition, x: number, y: number) {
+  if (isWin.value) {
+    return
+  }
+  if (col.color === EPieceType.empty) {
+    col.color = userColor.value
+
+    const win = positionWinCheck(board.value, x, y)
+    if (win) {
+      isWin.value = win
+      return
+    }
+
+    switch (userColor.value) {
+      case EPieceType.black:
+        userColor.value = EPieceType.white
+        break
+
+      case EPieceType.white:
+      default:
+        userColor.value = EPieceType.black
+        break
+    }
+  }
+}
+
+console.log(board.value)
+</script>
+
+<style lang="css">
+.coin {
+  width: 40px;
+  height: 40px;
+  position: relative;
+}
+
+.pill {
+  position: absolute;
+  z-index: 1;
+  border-radius: 20px;
+  height: 80%;
+  width: 80%;
+  left: 10%;
+  top: 10%;
+  box-shadow:
+    -2px 2px 4px 0px gray,
+    inset 1px 0px 4px 0px gray;
+}
+
+.white {
+  background-color: azure;
+}
+
+.black {
+  background-color: black;
+}
+
+.row {
+  display: flex;
+  justify-content: center;
+}
+
+.board {
+  position: relative;
+  background-color: darkseagreen;
+  width: fit-content;
+}
+
+.board-container {
+  display: flex;
+  justify-content: center;
+}
+</style>
---
+++ packages/pac-man/pac-board/src/board.ts
@@ -0,0 +1,82 @@
+export enum EPieceType {
+  'black' = -1,
+  'white' = 1,
+  'empty' = 0,
+}
+
+export interface IPosition {
+  color: EPieceType
+}
+
+export function genBoard() {
+  const board: IPosition[][] = []
+
+  for (let index = 0; index < boardSize; index++) {
+    const row: IPosition[] = []
+    for (let index = 0; index < boardSize; index++) {
+      row.push({
+        color: EPieceType.empty,
+      })
+    }
+    board.push(row)
+  }
+
+  return board
+}
+
+export function count(
+  board: IPosition[][],
+  x: number,
+  y: number,
+  [dx, dy]: [number, number],
+  pieceType: EPieceType,
+): number {
+  const piece = board[y][x]
+  if (piece.color === pieceType) {
+    return 1 + count(board, x + dx, y + dy, [dx, dy], pieceType)
+  }
+  return 0
+}
+
+const directions = [
+  [1, 0],
+  [1, 1],
+  [0, 1],
+  [-1, 1],
+]
+
+export function co(
+  board: IPosition[][],
+  x: number,
+  y: number,
+  pieceType: EPieceType,
+) {
+  return directions.some(([dx, dy]) => {
+    const [inversDx, inversDy] = [dx * -1, dy * -1]
+    const piece = board[y][x]
+    if (piece.color === pieceType) {
+      const countNumber =
+        1 +
+        count(board, x + dx, y + dy, [dx, dy], pieceType) +
+        count(
+          board,
+          x + inversDx,
+          y + inversDy,
+          [inversDx, inversDy],
+          pieceType,
+        )
+      if (countNumber >= 5) {
+        return true
+      }
+      return false
+    }
+  })
+}
+
+export function positionWinCheck(board: IPosition[][], x: number, y: number) {
+  const piece = board[y][x]
+
+  return co(board, x, y, piece.color)
+}
+
+export const boardSize = 2
